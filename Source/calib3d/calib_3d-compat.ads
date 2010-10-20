d2616d7e4d30d865833132316ff6449d31c04ad3
cederholm.lars <cederholm.lars@a4cdace2-5f73-c0cb-274a-12212bb6987c>
2010-10-20 13:05:10 +0000

    Calib3d compatibility changes between OpenCV 2.1 and 2.1.1

---
+++ Source/calib3d/calib_3d-compat.ads
@@ -0,0 +1,22 @@
+-----------------------------------------------------------------------
+-- Ada bindings for OpenCV 2.1.1 (from SVN 3 October 2010, rev. 3703)
+-- Developed as a master thesis project at Mälardalens Högskola
+-- OpenCV: http://opencv.willowgarage.com/
+-- Ada bindings : http://not_available.nope/
+-- License @ ./LICENSE (BSD license)
+-----------------------------------------------------------------------
+
+--Contact--------------------------------------------------------------
+-- Lars Cederholm, Niklas Pettersson
+-- Mälardalens Högskola, http://www.mdh.se/
+-- [lcm06001,npn06002]@student.mdh.se
+-----------------------------------------------------------------------
+
+--File-Info-------------------------------------------------------------
+-- calib_3d-compat.ads - compat_c.h
+-- Comments, Information, Other
+-----------------------------------------------------------------------
+
+package Calib_3D.Compat is
+
+end Calib_3D.Compat;
