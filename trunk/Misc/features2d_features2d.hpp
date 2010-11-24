403211f8ba0f7b61b6c019df96b89ca20c6c9831
cederholm.lars <cederholm.lars@a4cdace2-5f73-c0cb-274a-12212bb6987c>
2010-11-24 13:19:58 +0000

    Inline functions that need wrappers added in Misc

--- trunk/Misc/features2d_features2d.hpp
+++ trunk/Misc/features2d_features2d.hpp
@@ -5,51 +5,25 @@
 
 
 
- 
 
-CV_INLINE CvSURFPoint cvSURFPoint( CvPoint2D32f pt, int laplacian,
+
+ CvSURFPoint cvSURFPoint( CvPoint2D32f pt, int laplacian,
                                    int size, float dir CV_DEFAULT(0),
                                    float hessian CV_DEFAULT(0))
 ;
 
- 
-
-CVAPI(CvSURFParams) cvSURFParams( double hessianThreshold, int extended CV_DEFAULT(0) );
 
 
 
-CVAPI(void) cvExtractSURF( const CvArr* img, const CvArr* mask,
-                           CvSeq** keypoints, CvSeq** descriptors,
-                           CvMemStorage* storage, CvSURFParams params, int useProvidedKeyPts CV_DEFAULT(0)  );
-
-
- 
-
-CVAPI(CvMSERParams) cvMSERParams( int delta CV_DEFAULT(5), int min_area CV_DEFAULT(60),
-                           int max_area CV_DEFAULT(14400), float max_variation CV_DEFAULT(.25f),
-                           float min_diversity CV_DEFAULT(.2f), int max_evolution CV_DEFAULT(200),
-                           double area_threshold CV_DEFAULT(1.01),
-                           double min_margin CV_DEFAULT(.003),
-                           int edge_blur_size CV_DEFAULT(5) );
-
-
-CVAPI(void) cvExtractMSER( CvArr* _img, CvArr* _mask, CvSeq** contours, CvMemStorage* storage, CvMSERParams params );
-
-
- 
-
-CV_INLINE CvStarKeypoint cvStarKeypoint(CvPoint pt, int size, float response)
+ CvStarKeypoint cvStarKeypoint(CvPoint pt, int size, float response)
 ;
 
- 
 
-CV_INLINE CvStarDetectorParams cvStarDetectorParams(
+
+ CvStarDetectorParams cvStarDetectorParams(
     int maxSize CV_DEFAULT(45),
     int responseThreshold CV_DEFAULT(30),
     int lineThresholdProjected CV_DEFAULT(10),
     int lineThresholdBinarized CV_DEFAULT(8),
     int suppressNonmaxSize CV_DEFAULT(5))
 ;
-
-CVAPI(CvSeq*) cvGetStarKeypoints( const CvArr* img, CvMemStorage* storage,
-        CvStarDetectorParams params CV_DEFAULT(cvStarDetectorParams()));
--- trunk/Misc/src_precomp.hpp
+++ trunk/Misc/src_precomp.hpp
@@ -10,9 +10,3 @@
 
 static inline Point normalizeAnchor( Point anchor, Size ksize )
 ;
-
-void preprocess2DKernel( const Mat& kernel, vector<Point>& coords, vector<uchar>& coeffs );
-void crossCorr( const Mat& src, const Mat& templ, Mat& dst,
-                Size corrsize, int ctype,
-                Point anchor=Point(0,0), double delta=0,
-                int borderType=BORDER_REFLECT_101 );
