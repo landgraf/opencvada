ca93b9adac780b05cd07fe420f85dcb8bf0119b2
cederholm.lars <cederholm.lars@a4cdace2-5f73-c0cb-274a-12212bb6987c>
2010-11-24 13:32:56 +0000

    Wrappers generated for inline extern functions

---
+++ Misc/wrapper_parsed/legacy_legacy.hpp.hpp
@@ -0,0 +1,5 @@
+double icvSqDist2D32f (CvPoint2D32f pt1 , CvPoint2D32f pt2 );
+Cv3dTracker2dTrackedObject cv3dTracker2dTrackedObject (int id , CvPoint2D32f p );
+Cv3dTrackerTrackedObject cv3dTrackerTrackedObject (int id , CvPoint3D32f p );
+int iplWidth (const IplImage * img );
+int iplHeight (const IplImage * img );
