b01118ab37fc1af52caca44e9f762ca8acb56df5
ostmackan <ostmackan@a4cdace2-5f73-c0cb-274a-12212bb6987c>
2010-12-07 14:18:42 +0000

    Fix for camera in Opencv220 when using windows.

--- Source/samples/chess_demo.adb
+++ Source/samples/chess_demo.adb
@@ -1,5 +1,10 @@
+with Highgui; use Highgui;
+with Core; use Core;
+with Calib_3D;
+use Calib_3D;
+
 procedure Chess_Demo is
-   Capture : Highgui.Highgui_C.Cv_Capture_P;
+   Capture : Cv_Capture_P;
    Image   : Ipl_Image_P;
    Retval  : Integer;
    Corners : Cv_Point_2D_32F_Array (1 .. 49);
