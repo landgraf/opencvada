d9e812f3651380ed8e8de59ed96405157d08c78c
ostmackan <ostmackan@a4cdace2-5f73-c0cb-274a-12212bb6987c>
2010-12-08 13:06:56 +0000

    Fixed wrappers , should work now.

---
+++ Misc/wrapper_parsed/features2d_features2d.hpp.hpp
@@ -0,0 +1,3 @@
+ CvSURFPoint cvSURFPoint_wrap(CvPoint2D32f pt , int laplacian , int size , float dir , float hessian );
+ CvStarKeypoint cvStarKeypoint_wrap(CvPoint pt , int size , float response );
+ CvStarDetectorParams cvStarDetectorParams_wrap(int maxSize , int responseThreshold , int lineThresholdProjected , int lineThresholdBinarized , int suppressNonmaxSize );
---
+++ Misc/wrapper_parsed/highgui_highgui_c.h.hpp
@@ -0,0 +1,1 @@
+ int CV_FOURCC_wrap(char c1 , char c2 , char c3 , char c4 );
