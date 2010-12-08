799f1bc81a8ccbeea667ba9e953c495d4581b83c
cederholm.lars <cederholm.lars@a4cdace2-5f73-c0cb-274a-12212bb6987c>
2010-12-08 11:34:40 +0000

    Updated wrapped C functions to resolve name conflicts

--- trunk/Misc/wrapper_parsed/legacy_legacy.hpp.hpp
+++ trunk/Misc/wrapper_parsed/legacy_legacy.hpp.hpp
@@ -1,7 +1,5 @@
-#include <opencv2/legacy/legacy.hpp>
-
-double icvSqDist2D32f (CvPoint2D32f pt1 , CvPoint2D32f pt2 );
-Cv3dTracker2dTrackedObject cv3dTracker2dTrackedObject (int id , CvPoint2D32f p );
-Cv3dTrackerTrackedObject cv3dTrackerTrackedObject (int id , CvPoint3D32f p );
-int iplWidth (const IplImage * img );
-int iplHeight (const IplImage * img );
+ double icvSqDist2D32f_wrap(CvPoint2D32f pt1 , CvPoint2D32f pt2 );
+ Cv3dTracker2dTrackedObject cv3dTracker2dTrackedObject_wrap(int id , CvPoint2D32f p );
+ Cv3dTrackerTrackedObject cv3dTrackerTrackedObject_wrap(int id , CvPoint3D32f p );
+ int iplWidth_wrap(const IplImage * img );
+ int iplHeight_wrap(const IplImage * img );
