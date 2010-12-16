92949442d46b6924c5f1b6079e809f3461883dd3
ostmackan <ostmackan@a4cdace2-5f73-c0cb-274a-12212bb6987c>
2010-12-16 10:39:55 +0000

    Fixed errors in C/C++ wrappers.

--- trunk/Misc/wrapper_parsed/features2d_features2d.hpp.hpp
+++ trunk/Misc/wrapper_parsed/features2d_features2d.hpp.hpp
@@ -1,5 +1,6 @@
 #include <opencv2/opencv.hpp>
-
-CvSURFPoint cvSURFPoint_wrap(CvPoint2D32f pt , int laplacian , int size , float dir , float hessian );
- CvStarKeypoint cvStarKeypoint_wrap(CvPoint pt , int size , float response );
- CvStarDetectorParams cvStarDetectorParams_wrap(int maxSize , int responseThreshold , int lineThresholdProjected , int lineThresholdBinarized , int suppressNonmaxSize );
+extern "C" {
+  CvSURFPoint cvSURFPoint_wrap(CvPoint2D32f pt , int laplacian , int size , float dir , float hessian );
+  CvStarKeypoint cvStarKeypoint_wrap(CvPoint pt , int size , float response );
+  CvStarDetectorParams cvStarDetectorParams_wrap(int maxSize , int responseThreshold , int lineThresholdProjected , int lineThresholdBinarized , int suppressNonmaxSize );
+}
--- trunk/Misc/wrapper_parsed/highgui_highgui_c.h.hpp
+++ trunk/Misc/wrapper_parsed/highgui_highgui_c.h.hpp
@@ -1,3 +1,4 @@
 #include <opencv2/opencv.hpp>
-
-int CV_FOURCC_wrap(char c1 , char c2 , char c3 , char c4 );
+extern "C" {
+  int CV_FOURCC_wrap(char c1 , char c2 , char c3 , char c4 );
+}
