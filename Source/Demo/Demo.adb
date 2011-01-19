85d4f29ccfcc00826a8229d54a4492dbe928822f
ostmackan <ostmackan@a4cdace2-5f73-c0cb-274a-12212bb6987c>
2011-01-19 08:52:05 +0000

    Changed project to a library.

---
+++ Source/Demo/Demo.adb
@@ -0,0 +1,27 @@
+with Highgui; use Highgui;
+with Core; use Core;
+with Core.Operations;
+use Core.Operations;
+with Imgproc;
+use Imgproc;
+with Imgproc.Operations;
+use Imgproc.Operations;
+with Video.Background_Segm;
+with Video.Blob_Track;
+with Video.Tracking; -- empty
+use Video.Tracking;
+with Objdetect;
+with Features_2d;
+with Ff_Opencv;
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Calendar;
+with Ada.Numerics.Generic_Elementary_Functions; use Core.Long_Float_Numerics;
+
+
+with GNAT.OS_Lib; -- kom ihåg
+
+-- tiden funkar nog inte!
+procedure Main is
+begin
+   null;
+end Main;
