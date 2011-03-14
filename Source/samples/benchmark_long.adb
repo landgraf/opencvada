961a82ea26cdd5b016feb805dedb0a9c06e3b062
ostmackan <ostmackan@a4cdace2-5f73-c0cb-274a-12212bb6987c>
2011-03-14 14:52:51 +0000

    benchmark fixes.

--- Source/samples/benchmark_long.adb
+++ Source/samples/benchmark_long.adb
@@ -8,18 +8,22 @@ with Interfaces; use Interfaces;
 with Imgproc; use Imgproc;
 with Imgproc.Operations; use Imgproc.Operations;
 with Ada.Unchecked_Conversion;
-
+with Core.Mat;
 
 
 -- benchmark_long file.avi
 procedure Benchmark_Long is
+   subtype Lf_Circle is Core.Cv_32f_Array (0 .. 2);
+   package Matrix is new Core.Mat (Float);
+
    Capture : aliased cv_Capture_Ptr;
    Frame   : Ipl_Image_Ptr;
    Frames  : Interfaces.Integer_64 := 0;
    Bw,Recolor      : aliased Ipl_Image_Ptr;
-   Storage         : aliased Cv_Mem_Storage_Ptr;
+   Storage         : aliased Matrix.Cv_Mat_Ptr;
    Circles : Cv_Seq_Ptr;
-   subtype Lf_Circle is Core.Cv_32f_Array (0 .. 2);
+   C_Array : Matrix.Element_Array(0 .. (10000*3)-1) := (others => 0.0);
+
    Circle  : Lf_Circle;
 
    type Lf_Circle_Ptr is access Lf_Circle;
@@ -28,6 +32,8 @@ procedure Benchmark_Long is
                                                             Source => Cv_Mem_Storage_Ptr);
    function To_Lf_Circle_Ptr is new Ada.Unchecked_Conversion    (Target => Lf_Circle_Ptr,
                                                                  Source => Cv_Void_Ptr);
+
+   Loop_N : Integer := 0;
 begin
    if Argument_Count > 0 then
       Capture := Cv_Create_File_Capture (Argument(1));
@@ -39,16 +45,20 @@ begin
       Frame := Cv_Query_Frame (Capture);
       --waste a bit of time
       exit when Frame = null;
-      Storage := Cv_Create_Mem_Storage(0);
+      Storage := Matrix.Cv_Create_Mat (1, 10000, Cv_32f, 3, C_Array);
       Bw := Cv_Create_Image (Cv_Get_Size (Frame), 8, 1);
       Recolor := Cv_Create_Image (Cv_Get_Size (Frame), 8, 3);
       Cv_Cvt_Color (Frame, Bw, Cv_Bgr2gray);
       Cv_Canny (Bw, Bw, 10.0, 240.0);
-      Circles := Imgproc.Operations.Cv_Hough_Circles (Bw, To_Void_Ptr (Storage), Cv_Hough_Gradient, 1.0, Long_Float (Bw.all.Height / 10), 300.0, 30.0);
+      Circles := Imgproc.Operations.Cv_Hough_Circles (Bw, Matrix.To_Void_Ptr (Storage), Cv_Hough_Gradient, 1.0, Long_Float (Bw.all.Height / 10), 300.0, 30.0);
       Cv_Cvt_Color (Bw, Recolor, Cv_Gray2bgr);
-      for I in Integer range 0 .. Circles.all.Total - 1 loop
-         Circle := To_Lf_Circle_Ptr (Cv_Get_Seq_Elem (Circles, I)).all;
-         Cv_Draw_Circle (Recolor, Cv_Create_Point (Cv_Round(Circle(0)), Cv_Round(Circle(1))), Cv_Round(Circle(2)), Cv_Rgb (255, 0, 0));
+--        C_Array := new Matrix.element_Array(0 .. storage.all.Rows - 1);
+--        C_Array.all := Matrix.Cv_Get_Mat_Data (Storage);
+--        for I in Integer range 0 .. Storage.all.Rows - 1 loop
+      while Loop_N < C_Array'Length loop
+         Circle := (C_Array (Loop_N), C_Array (Loop_N + 1), C_Array (Loop_N + 2));
+         Cv_Draw_Circle (Recolor, Cv_Create_Point (Cv_Round (Circle (0)), Cv_Round (Circle (1))), Cv_Round (Circle (2)), Cv_Rgb (255, 0, 0));
+         Loop_N := Loop_N + 3;
       end loop;
 --        Put_Line (Circles.all.Total'Img);
 --        Cv_Show_Image ("hej", Recolor);
@@ -56,7 +66,7 @@ begin
       Frames := Frames + 1;
       Cv_Release_Image (Bw'Access);
       Cv_Release_Image (Recolor'Access);
-      Cv_Release_Mem_Storage (Storage'Access);
+      Matrix.Cv_Release_Mat (Storage);
    end loop;
    Put_Line("Frames: " & Frames'Img);
    Cv_Release_Capture (Capture'Access);
