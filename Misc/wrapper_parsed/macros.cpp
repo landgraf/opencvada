3426ec893d75c245cf8a11a1f60b7d8af5be57c1
cederholm.lars <cederholm.lars@a4cdace2-5f73-c0cb-274a-12212bb6987c>
2011-05-10 11:53:03 +0000

    Some missing stuff wrapped in delicious C code

--- Misc/wrapper_parsed/macros.cpp
+++ Misc/wrapper_parsed/macros.cpp
@@ -60,3 +60,96 @@ void Cv_Read_Edge (CvPoint * pt1, CvPoint * pt2, CvChainPtReader * reader)
   CV_READ_EDGE( (*pt1), (*pt2), (*reader) );
 }
 
+int Cv_Is_Mat_Hdr(CvMat *mat) {
+  return CV_IS_MAT_HDR(mat);
+}
+
+int Cv_Is_Mat(CvMat *mat) {
+  return CV_IS_MAT(mat);
+}
+
+int Cv_Is_Mask_Arr(CvMat *mat) {
+  return CV_IS_MASK_ARR(mat);
+}
+
+int Cv_Are_Types_Eq(CvMat *mat1, CvMat *mat2) {
+  return CV_ARE_TYPES_EQ(mat1, mat2);
+}
+
+int Cv_Are_Cns_Eq(CvMat *mat1, CvMat *mat2) {
+  return CV_ARE_CNS_EQ(mat1, mat2);
+}
+
+int Cv_Are_Depths_Eq(CvMat *mat1, CvMat *mat2) {
+  return CV_ARE_DEPTHS_EQ(mat1, mat2);
+}
+
+int Cv_Are_Sizes_Eq(CvMat *mat1, CvMat *mat2) {
+  return CV_ARE_SIZES_EQ(mat1, mat2);
+}
+
+int Cv_Is_Mat_Const(CvMat *mat) {
+  return CV_IS_MAT_CONST(mat);
+}
+
+int Cv_Mat_Cn(int flags) {
+  return CV_MAT_CN(flags);
+}
+
+void *Cv_Mat_Elem_Ptr_Fast(CvMat *mat, int row, int col, int pix_size) {
+  return CV_MAT_ELEM_PTR_FAST(*mat, row, col, pix_size);
+}
+
+void *Cv_Mat_Elem_Ptr(CvMat *mat, int row, int col) {
+  return CV_MAT_ELEM_PTR(*mat, row, col);
+}
+
+int Cv_Is_Mat_ND_Hdr(CvMatND *mat) {
+  return CV_IS_MATND_HDR(mat);
+}
+
+int Cv_Is_Mat_ND(CvMatND *mat) {
+  return CV_IS_MATND(mat);
+}
+
+int Cv_Is_Sparse_Mat_Hdr(CvSparseMat *mat) {
+  return CV_IS_SPARSE_MAT_HDR(mat);
+}
+
+int Cv_Is_Sparse_Mat(CvSparseMat *mat) {
+  return CV_IS_SPARSE_MAT(mat);
+}
+
+int Cv_Is_Sparse_Hist(CvHistogram *hist) {
+  return CV_IS_SPARSE_HIST(hist);
+}
+
+int Cv_Maketype(int depth, int channels) {
+  return CV_MAKETYPE(depth, channels);
+}
+
+int func_ptr_test(int (*func) (int, int)) {
+  return func(30, 20);
+}
+
+int bounded_array_test(int arr[5]) {
+  int max = arr[0];
+
+  for (int i = 1; i < 10; i++) {
+    if(arr[i] > max)
+      max = arr[i];
+  }
+  return max;
+}
+
+int unbounded_array_test(int *arr, int length) {
+  int max = arr[0];
+
+  for(int i = 1; i < length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+      arr[i] = 0;
+    }
+  }
+  return max;
+}
--- Misc/wrapper_parsed/macros.hpp
+++ Misc/wrapper_parsed/macros.hpp
@@ -4,7 +4,7 @@ extern "C" {
   void cvNextSeqElem(int elem_size, CvSeqReader * reader);
   int Cv_Is_Haar_Classifier(CvHaarClassifierCascade * haar);
   void Cv_Next_Line_Point(CvLineIterator line_iterator);
-  void * Cv_Mat_Elem_Ptr_Fast(CvMat mat, int row, int col, int pix_size);
+  //void * Cv_Mat_Elem_Ptr_Fast(CvMat mat, int row, int col, int pix_size);
   void Cv_Write_Seq_Elem_Var(void * elem_ptr, CvSeqWriter writer);
   void Cv_Prev_Seq_Elem(int elem_size, CvSeqReader reader);
   void Cv_Rev_Read_Seq_Elem(void * elem, CvSeqReader reader);
@@ -12,4 +12,35 @@ extern "C" {
   void Cv_Read_Chain_Point(CvPoint * _pt, CvChainPtReader reader);
   void Cv_Next_Graph_Edge( CvGraphEdge * edge, CvGraphVtx * vertex);
   void Cv_Read_Edge (CvPoint * pt1, CvPoint * pt2, CvChainPtReader * reader);
+
+  int Cv_Maketype(int depth, int channels);
+
+  int Cv_Is_Mat_Hdr(CvMat *mat);
+  int Cv_Is_Mat(CvMat *mat);
+  int Cv_Is_Mask_Arr(CvMat *mat);
+  int Cv_Are_Types_Eq(CvMat *mat1, CvMat *mat2);
+  int Cv_Are_Cns_Eq(CvMat *mat1, CvMat *mat2);
+  int Cv_Are_Depths_Eq(CvMat *mat1, CvMat *mat2);
+  int Cv_Are_Sizes_Eq(CvMat *mat1, CvMat *mat2);
+  int Cv_Is_Mat_Const(CvMat *mat);
+  int Cv_Mat_Cn(int flags);
+  void *Cv_Mat_Elem_Ptr_Fast(CvMat *mat, int row, int col, int pix_size);
+  void *Cv_Mat_Elem_Ptr(CvMat *mat, int row, int col);
+
+  int Cv_Is_Mat_ND_Hdr(CvMatND *mat);
+  int Cv_Is_Mat_ND(CvMatND *mat);
+
+  int Cv_Is_Sparse_Mat_Hdr(CvSparseMat *mat);
+  int Cv_Is_Sparse_Mat(CvSparseMat *mat);
+  int Cv_Is_Sparse_Hist(CvHistogram *hist);
+
+  int func_ptr_test(int (*func) (int, int));
+  int bounded_array_test(int arr[5]);
+  int unbounded_array_test(int *arr, int length);
+
+  typedef int (*func_ptr) (int, int);
+  typedef struct {
+    func_ptr func;
+    int (*func_ptr2) (int, int);
+  } func_struct;
 }
