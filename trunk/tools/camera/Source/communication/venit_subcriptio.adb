f8881787f98dfd384ca49a38dbafbd8fcd1dd65f
cederholm.lars <cederholm.lars@a4cdace2-5f73-c0cb-274a-12212bb6987c>
2011-05-11 09:35:36 +0000

    Image header thingy

--- trunk/tools/camera/Source/communication/venit_subcriptio.adb
+++ trunk/tools/camera/Source/communication/venit_subcriptio.adb
@@ -14,4 +14,42 @@ package body Venit_Subcriptio is
       Temp.Flags := 2#0001#;
       return Temp;
    end Ping;
+
+   function Create_Image_Header (Cols   : Header_Columns;
+                                 Rows   : Header_Rows;
+                                 Depth  : Header_Color_Depth := Color_36_Bit;
+                                 Origin : Header_Bit := False;
+                                 Float  : Header_Bit := False)
+                                 return Image_Header is
+      Header : Image_Header;
+   begin
+      Header.Columns := Cols;
+      Header.Rows := Rows;
+      Header.Depth := Depth;
+      Header.Origin := Origin;
+      Header.Float := Float;
+--        Header.Reserved := (others => False);
+
+      return Header;
+   end Create_Image_Header;
+
+   function To_Frame_Header (Src : Image_Header) return Frame_Header is
+      Dest : Frame_Header;
+      Temp : Frame_Data (0 .. 19);
+      for Temp'Address use Src'Address;
+   begin
+      Dest.Data := Temp;
+      Dest.Length := Image_Header_Size;
+      return Dest;
+   end To_Frame_Header;
+
+   function To_Image_Heaeder (Src    : Frame_Header;
+                              Offset : Integer := 0)
+                              return Image_Header is
+      Temp : Frame_Data (1 .. Image_Header_Size) := Src (Offset .. Offset + Image_Header_Size);
+      Dest : Image_Header;
+      for Dest'Address use Temp'Address;
+   begin
+      return Dest;
+   end To_Image_Header;
 end Venit_Subcriptio;
